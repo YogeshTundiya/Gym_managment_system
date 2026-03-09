f7900db552c86de63bb0e199133c2f464be06ba6
YogeshTundiya <yogeshtundiya945@gmail.com>
2026-03-09 16:39:54 +0530

    Fix: Replace deprecated @studio-freight/react-lenis with modern lenis package for React 19 compatibility

--- client/src/components/providers/SmoothScrolling.tsx
+++ client/src/components/providers/SmoothScrolling.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactLenis } from "@studio-freight/react-lenis";
+import { ReactLenis } from "lenis/react";
 import { usePathname } from "next/navigation";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
